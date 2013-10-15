5aa75b0a64567523372d7a78f5f0424e9c2127f7
MengHuan Yu <cebrusfs@gmail.com>
2013-10-15 22:44:38 +0800

    add code_generate interface in main file

--- src/AcDc.rb
+++ src/AcDc.rb
@@ -7,22 +7,30 @@ class AcDcCompiler
 
     def initialize(argv)
         raise "can't open the source file\n" unless File.readable? argv[0]
-        @source = File.open(argv[0], "r")
-        @target = File.open(argv[1], "w")
-        @scanner = Scanner.new(@source)
+        @source_file = File.open(argv[0], "r")
+        @target_file = File.open(argv[1], "w")
+        @scanner = Scanner.new(@source_file)
         @token_list = []
+        @syntax_tree = nil
     end
     
-    def parse
+    def get_input
         while token = @scanner.getToken
             @token_list.push(token)
         end
         @token_list.push(Token.new(:T_eof))
-        test = Prog.new(@token_list)
-        test.parse
-        # test.trace
+    end 
+
+    def parse
+        @syntax_tree = Prog.new(@token_list)
+        @syntax_tree.parse
     end
     
+    def code_generate
+        #result = @syntax_tree.code_generate 
+        #puts result
+        # TODO: output to target_file
+    end
 end
 
 ###################################
@@ -37,8 +45,9 @@ def main
         raise "Usage: #{$0} source_file target_file" unless ARGV.length == 2
 
         myAcDc = AcDcCompiler.new(ARGV)
+        myAcDc.get_input
         myAcDc.parse
-    
+        myAcDc.code_generate 
     rescue Exception => e    # Compile failed.
         puts e.message
     end
